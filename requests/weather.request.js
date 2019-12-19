b1dbcceb1066e56bf6660c55920ea34fac9fe5c3
Safarov <elkhan.safarov10@yandex.com>
2019-12-19 15:59:52 +0500

    Вывод данных

--- requests/weather.request.js
+++ requests/weather.request.js
@@ -1,31 +1,36 @@
-const rp = require('request-promise');
+const rp = require('request-promise')
 
 module.exports = async function(city = '') {
-    if(!city) {
-        throw new Error('Имя города не может быть пустым');
+  if (!city) {
+    throw new Error('Имя города не может быть пустым')
+  }
+
+  const KEY = '8dc41ecd6844060483c042875ced6e05'
+  const uri = 'http://api.openweathermap.org/data/2.5/weather'
+
+  const options = {
+    uri,
+    qs: {
+      appid: KEY,
+      q: city,
+      units: 'imperial'
+    },
+    json: true
+  }
+
+  try {
+    const data = await rp(options)
+    const celsius = (data.main.temp - 32) * 5/9
+
+    return {
+      weather: `${data.name}: ${celsius.toFixed(0)}`,
+      error: null
     }
-    const KEY = '8dc41ecd6844060483c042875ced6e05';
-    const uri = 'http://api.openweathermap.org/data/2.5/weather';
-    const options = {
-        uri,
-        qs: {
-            appid: KEY,
-            q: city,
-            units: 'imperial'
-        },
-        json: true
+  } catch (error) {
+    // console.log(error)
+    return {
+      weather: null,
+      error: error.error.message
     }
-    try {
-        const response = await rp(options);
-        const celsius = (data.main.temp - 32) * 5 / 9;
-        return {
-            weather: `${data.name}: ${celsius.toFixed(0)}`,
-            error: null
-        }
-    } catch(error) {
-        return {
-            weather: null,
-            error: error.error.message
-        }
-    }
+  } 
 }
