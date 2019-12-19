b801675c0b64a9e480602d3e7f44687cdfe948b9
Safarov <elkhan.safarov10@yandex.com>
2019-12-19 14:04:05 +0500

    Обработка формы

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',(req, res) => {
+    res.render('index');
+});
+
+app.post('/',(req, res) => {
+    const { city } = req.body;
     res.render('index');
 });
 
